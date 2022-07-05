aa41277534f7039238b7585267d6153c480bfbb2
Goldfinch <facu_vila@hotmail.com>
2022-07-05 14:58:52 -0300

    Cambios en la logica de la barra de busqueda

--- potterpedia.js
+++ potterpedia.js
@@ -1,7 +1,44 @@
-const cargarPersonajes =async()=>{
-    const estudiantes = await fetch("http://hp-api.herokuapp.com/api/characters/students")
-    console.log(estudiantes);
+const inputBuscador = document.getElementById ('buscador');
+
+let estudiantes = [];
+
+const cargarEstudiantes = async () => {
+
+    try {
+
+        const respuesta = await fetch('http://hp-api.herokuapp.com/api/characters/students');
+        console.log(respuesta);
+
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+            let estudiantes = '';
+            datos.forEach(estudiante => {
+                console.log(estudiante);
+                estudiantes += `<h1>${estudiante}</h1>`;
+            });
+
+        }else if (respuesta.status === 404) {
+            console.log('La pagina no funciona');
+
+        } else {
+            console.log('Error desconocido');
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 
-cargarPersonajes()
+
+cargarEstudiantes();
+
+inputBuscador.addEventListener('keyup', contenidoDelBuscador =>{
+    let texto = contenidoDelBuscador.target.value;
+    const filtrarEstudiantes = estudiantes.filter (estudiante => {
+        return estudiante.name.contain(searchString)
+    })
+    console.log(filtrarEstudiantes);
+})
+
